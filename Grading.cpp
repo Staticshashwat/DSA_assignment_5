f463be836544dd7d1fe8bf60ffdb2cb902be19b2
Staticshashwat <118439884+Staticshashwat@users.noreply.github.com>
2023-02-01 21:24:04 +0530

    Update Grading.cpp

--- Grading.cpp
+++ Grading.cpp
@@ -1,33 +1,37 @@
-#include<iostream>
+https://www.hackerrank.com/challenges/grading/problem?isFullScreen=true
+
+
+#include <bits/stdc++.h>
 using namespace std;
+
 int main()
 {
-    int n,s,k,r;
-    cin>>n;
-    int a[n];
-    for(int i=0;i<n;i++)
+int n,i;
+cin>> n;
+int grade[n];
+for(i=0;i<n;i++)
+{
+    cin>>grade[i];
+}    
+for(i=0;i<n;i++)
+{
+    if(grade[i]<38)
     {
-        cin>>a[i];
+        cout<<grade[i]<<endl;
     }
-    for(int i=0;i<n;i++)
+    if(grade[i]>=38)
     {
-        if(a[i]>=38)
+        int d=grade[i]/5;
+        int r=d+1;
+        int s=r*5;
+        if(s-grade[i]<3)
         {
-        k=a[i]/5;
-        s=k+1;
-        r=s*5;
-        if(r-a[i]<3)
-        {
-            cout<<r<<endl;
+            cout<<s<<endl;
         }
-           else {
-           cout<<a[i]<<endl;
-           } 
-        }
-        if(a[i]<38)
+        else 
         {
-            cout<<a[i]<<endl;
+            cout<<grade[i]<<endl;
         }
     }
-    
+}
 }
