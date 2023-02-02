517ba99490c8125f16a6b595da9d43be6af6f207
Staticshashwat <118439884+Staticshashwat@users.noreply.github.com>
2023-02-02 15:21:22 +0530

    Update kangaroo.cpp

--- kangaroo.cpp
+++ kangaroo.cpp
@@ -1,14 +1,22 @@
+https://www.hackerrank.com/challenges/kangaroo/problem?isFullScreen=true
+
+
 #include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    int x1, v1, x2, v2;
+    long x1,v1,x2,v2;
     cin>>x1>>v1>>x2>>v2;
-    if (v1>v2&&(x2-x1)%(v1-v2)==0)
+    if(x2>x1 && v1>v2 && ((x2-x1)%(v1-v2))==0)
     {
         cout<<"YES";
     }
-    else {
-    cout<<"NO";
+    else
+    {
+        cout<<"NO";
     }
 }
+    
+   
+    
+
